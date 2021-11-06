54627a9bb2cf82c5a20be94601f4cb1dbb644d63
Casper Kristiansson <36133918+CasperKristiansson@users.noreply.github.com>
2021-11-06 16:11:02 +0100

    Change stars to % width, add adaptive boxShadow

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,6 @@
     var intViewportWidth = window.innerWidth;
     var intViewportHeight = window.innerHeight;
 
-    clearHtml();
-    
-    window.addEventListener('resize', staticStars);
-
     for (let i = 0; i < 300; i++) {
         rand = Math.random() * 5;
         stars[i] = document.createElement("div");
@@ -15,16 +11,20 @@
 
         stars[i].style.width = rand + "px";
         stars[i].style.height = rand + "px";
-        stars[i].style.top = Math.random() * intViewportHeight + "px";
-        stars[i].style.left = Math.random() * intViewportWidth + "px";
+        stars[i].style.top = Math.random() * 80 + "%";
+        stars[i].style.left = Math.random() * 100 + "%";
 
         stars[i].style.marginTop= "0";
         stars[i].style.borderRadius = "60%";
-        stars[i].style.position = "relative";
+        stars[i].style.position = "absolute";
 
         if (rand < 2) stars[i].style.backgroundColor = "yellow";
+        else if (rand < 3) stars[i].style.backgroundColor = "#f8f8ba";
         else stars[i].style.backgroundColor = "white";
-        stars[i].style.boxShadow = "0 0 10px 0.2px";
+
+        if (rand < 2) stars[i].style.boxShadow = "0 0 3px 0.2px";
+        else if (rand < 3) stars[i].style.boxShadow = "0 0 5px 0.2px";
+        else stars[i].style.boxShadow = "0 0 10px 0.2px";
 
         if (rand < 1) rand = Math.random() * (1 - 0.5) + 0.5;;
         stars[i].style.animation = "glow " + rand + 1 + "s linear infinite alternate";
