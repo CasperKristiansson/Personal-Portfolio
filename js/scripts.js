7b0ee5bf59cc9a9c7fcf524e9b51fbff769707ab
Casper Kristiansson <36133918+CasperKristiansson@users.noreply.github.com>
2021-11-06 10:20:58 +0100

    Fix stars length, Add mobile for timeline

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,48 @@
 (function staticStars() {
-    window.addEventListener('resize', clearHtml);
-    window.addEventListener('resize', staticStars);
 
     var rand, stars = [];
     var intViewportWidth = window.innerWidth;
-    var intViewportHeight = window.innerHeight -435;
+    var intViewportHeight = window.innerHeight - 460;
 
-    for (let i = 0; i <= 300; i++) {
+    clearHtml(intViewportWidth);
+    
+    console.log(document.getElementsByClassName("stars")[0].children.length);
+    console.log("Size")
+    console.log(intViewportWidth*2);
+    
+    window.addEventListener('resize', staticStars);
+
+    for (let i = 0; i < intViewportWidth / 3; i++) {
         rand = Math.random() * 5;
         stars[i] = document.createElement("div");
         stars[i].id = "star-id-" + i;
+
         stars[i].style.width = rand + "px";
         stars[i].style.height = rand + "px";
+        stars[i].style.top = Math.random() * intViewportHeight * 1.7 + "px";
+        stars[i].style.left = Math.random() * intViewportWidth + "px";
+
         stars[i].style.marginTop= "0";
         stars[i].style.borderRadius = "60%";
-        stars[i].style.backgroundColor = "white";
-        stars[i].style.position = "relative";
-        stars[i].style.top = Math.random() * intViewportHeight + "px";
-        stars[i].style.left = Math.random() * intViewportWidth + "px";
+        stars[i].style.position = "absolute";
+
+        if (rand < 2) stars[i].style.backgroundColor = "yellow";
+        else stars[i].style.backgroundColor = "white";
+        stars[i].style.boxShadow = "0 0 10px 0.2px";
+
+        if (rand < 1) rand = Math.random() * (1 - 0.5) + 0.5;;
         stars[i].style.animation = "glow " + rand + 1 + "s linear infinite alternate";
 
         document.getElementsByClassName("stars")[0].appendChild(stars[i]);
     }
 })();
 
-function clearHtml() {
-    for (let i = 0; i <= 300; i++) {
-        document.getElementById("star-id-" + i).remove();
+function clearHtml(intViewportWidth) {
+    for (let i = 0; i < intViewportWidth * 2; i++) {
+        try {
+            document.getElementById("star-id-" + i).remove();
+        } catch (e) {
+            break;
+        }
     }
-}
+}   
