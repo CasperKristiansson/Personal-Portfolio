574755b116a9ec11e1747e50ad84bd7394eb15f8
Casper Kristiansson <casper.kristiansson@yahoo.se>
2021-11-21 16:09:08 +0100

    Updated Scroll Value

--- js/scripts.js
+++ js/scripts.js
@@ -34,7 +34,7 @@ $("#prev").on("click", function () {
     if (window.innerWidth < 500) {
         $("#menu ul").animate(
             {
-                scrollLeft: `-=${window.innerWidth*0.96}`,
+                scrollLeft: `-=${window.innerWidth}`,
             },
             300,
             "swing"
@@ -55,7 +55,7 @@ $("#next").on("click", function () {
     if (window.innerWidth < 500) {
         $("#menu ul").animate(
             {
-                scrollLeft: `+=${window.innerWidth*0.96}`,
+                scrollLeft: `+=${window.innerWidth}`,
             },
             300,
             "swing"
