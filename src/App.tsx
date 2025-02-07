06e759a3a411caf4cb5cc89a27c6a041c3aa2e08
Casper Kristiansson <casper.kristiansson@yahoo.se>
2025-02-08 00:32:43 +0100

    Add About section and integrate Prettier with Tailwind CSS support

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { About } from "./components/About";
 import { Hero } from "./components/Hero";
 
 export const App: React.FC = () => {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 };
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export const Hero: React.FC = () => {
-  return <></>;
+  return (
+    <div className="parallax flex h-[80vh] items-center justify-center">
+      <h1 className="typewriter font-mono text-5xl font-bold text-white md:text-7xl">
+        Casper Kristiansson
+      </h1>
+    </div>
+  );
 };
