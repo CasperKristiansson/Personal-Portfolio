98143b3fee6d0c2fe2e7c63507de95f5c2b4e2bb
Casper Kristiansson <casper.kristiansson@yahoo.se>
2025-02-20 22:01:59 +0100

    Add Rewards component and update Skills component colors for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Education } from "./components/Education";
 import { Experience } from "./components/Experience";
 import { Hero } from "./components/Hero";
 import { Projects } from "./components/Projects";
+import { Rewards } from "./components/Rewards";
 import { Skills } from "./components/Skills";
 
 export const App: React.FC = () => {
@@ -16,6 +17,7 @@ export const App: React.FC = () => {
       <Education />
       <Skills />
       <Certificates />
+      <Rewards />
     </>
   );
 };
