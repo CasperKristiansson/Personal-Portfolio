7d7d424e9ed5200c877be2d745a1ef9d522ebf5c
Casper Kristiansson <casper.kristiansson@yahoo.se>
2026-01-04 10:48:33 +0100

    refactor: update ESLint configuration to use defineConfig and improve settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,20 @@
+import { defineConfig, globalIgnores } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+export default defineConfig([
+  globalIgnores(["dist/**"]),
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    name: "vite-react",
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: globals.browser,
     },
     plugins: {
@@ -25,4 +29,4 @@ export default tseslint.config(
       ],
     },
   },
-);
+]);
